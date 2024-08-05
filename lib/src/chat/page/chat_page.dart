35613dafdddf7b2cf3d3e94373439d6ea7bddb3f
pump9918 <hoyeon9918@gmail.com>
2024-08-05 17:07:54 +0900

    Feat: 채팅 모델 직렬화

--- lib/src/chat/page/chat_page.dart
+++ lib/src/chat/page/chat_page.dart
@@ -1,9 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:nero_app/src/chat/controller/chat_controller.dart';
 import 'package:nero_app/src/common/components/app_font.dart';
+import 'package:nero_app/src/common/components/user_temperature_widget.dart';
 import 'package:nero_app/src/common/layout/common_layout.dart';
 
 class ChatPage extends GetView<ChatController> {
@@ -12,10 +15,87 @@ class ChatPage extends GetView<ChatController> {
   @override
   Widget build(BuildContext context) {
     return CommonLayout(
-      appBar: AppBar(),
-      body: const Column(
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        title: Obx(
+          () => Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AppFont(
+                controller.opponentUser.value.nickname ?? '',
+                fontWeight: FontWeight.bold,
+              ),
+              const SizedBox(width: 6),
+              UserTemperatureWidget(
+                temperature: controller.opponentUser.value.temperature ?? 0,
+                isSimpled: true,
+              ),
+            ],
+          ),
+        ),
+        actions: const [
+          SizedBox(width: 50),
+        ],
+      ),
+      body: Column(
         children: [
           _HeaderItemInfo(),
+          Spacer(),
+          _TextFieldWidget(),
+          KeyboardVisibilityBuilder(builder: (context, visible) {
+            return SizedBox(
+                height: visible
+                    ? MediaQuery.of(context).viewInsets.bottom
+                    : Get.mediaQuery.padding.bottom);
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+class _TextFieldWidget extends GetView<ChatController> {
+  const _TextFieldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(Radius.circular(30.0))
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(Radius.circular(30.0))
+                ),
+                border: InputBorder.none,
+                hintStyle: TextStyle(color: Color(0xff696D75)),
+                hintText: '메세지 보내기',
+                contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                fillColor: Color(0xff2B2E32),
+              ),
+              maxLines: null,
+              onSubmitted: (value) {},
+            ),
+          ),
+          GestureDetector(
+            onTap: () async {},
+            behavior: HitTestBehavior.translucent,
+            child: Container(
+              padding: const EdgeInsets.all(7),
+              child: SvgPicture.asset('assets/svg/icons/icon_sender.svg'),
+            ),
+          )
         ],
       ),
     );
