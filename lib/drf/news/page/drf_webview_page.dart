5b5c58268776fca3947e79a3bce60e450e37d366
pump9918 <hoyeon9918@gmail.com>
2024-09-02 18:57:19 +0900

    Feat: 크롤링 최신 뉴스 적용

---
+++ lib/drf/news/page/drf_webview_page.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class DrfWebviewPage extends StatefulWidget {
+  final String url;
+
+  const DrfWebviewPage({Key? key, required this.url}) : super(key: key);
+
+  @override
+  State<DrfWebviewPage> createState() => _WebviewPageState();
+}
+
+class _WebviewPageState extends State<DrfWebviewPage> {
+  WebViewController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..addJavaScriptChannel('Test', onMessageReceived: (JavaScriptMessage message) {
+        Map<String, dynamic> data = json.decode(message.message);
+        print("javascript func message: " + data['action'].toString());
+        if (data['action'] == "download") {
+          print("javascript func download 실행");
+          print("value: " + data['value'].toString());
+        }
+      })
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left, color: Colors.white),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller!),
+      ),
+    );
+  }
+}
--- lib/drf/product/home/page/drf_home_page.dart
+++ lib/drf/product/home/page/drf_home_page.dart
@@ -1,31 +1,50 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:nero_app/drf/news/page/drf_news_list_page.dart';
 import 'package:nero_app/drf/product/list/page/drf_product_list_page.dart';
 import 'package:nero_app/src/common/components/app_font.dart';
+import 'package:nero_app/src/common/layout/common_layout.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DrfHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return CommonLayout(
       appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
         leadingWidth: Get.width * 0.6,
         leading: Padding(
           padding: const EdgeInsets.only(left: 25),
-          child: Row(children: [
-            const AppFont(
-              '홈',
-              fontWeight: FontWeight.bold,
-              size: 20,
-              color: Colors.white,
-            ),
-            const SizedBox(width: 10),
-          ]),
+          child: Row(
+            children: [
+              const AppFont(
+                '홈',
+                fontWeight: FontWeight.bold,
+                size: 20,
+                color: Colors.white,
+              ),
+              const SizedBox(width: 10),
+            ],
+          ),
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout,color: Colors.white,),
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
             onPressed: () async {
               SharedPreferences prefs = await SharedPreferences.getInstance();
               await prefs.remove('accessToken');
@@ -35,7 +54,30 @@ class DrfHomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: DrfProductListPage(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2, // 비율에 따라 flex를 조정하여 페이지 높이 조절
+            child: DrfProductListPage(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 13),
+            child: Text(
+              '정신과 최신 뉴스',
+              style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
+                  color: Colors.white),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: DrfNewsListPage(),
+          ),
+        ],
+      ),
     );
   }
 }
