666c7cef1415ec9e2e05d035f9b95c6297a42e6e
pump9918 <hoyeon9918@gmail.com>
2024-09-16 15:36:36 +0900

    #7 Feat: 뉴스 리스트 페이지

--- lib/develop/home/news/page/news_list_page.dart
+++ lib/develop/home/news/page/news_list_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nero_app/develop/common/components/custom_detail_app_bar.dart';
 import 'package:nero_app/develop/home/news/model/news.dart';
 import 'package:nero_app/develop/home/news/page/news_web_view_page.dart';
@@ -27,13 +28,12 @@ class _NewsListPageState extends State<NewsListPage> {
   }
 
   Future<void> _fetchInitialNews() async {
-    // 초기 뉴스 로드
     await _fetchNewsPage(_currentPage);
   }
 
   void _onScroll() {
     if (_scrollController.position.pixels >=
-            _scrollController.position.maxScrollExtent &&
+        _scrollController.position.maxScrollExtent &&
         !_isLoading &&
         _hasMore) {
       _fetchNewsPage(_currentPage + 1);
@@ -67,8 +67,8 @@ class _NewsListPageState extends State<NewsListPage> {
   }
 
   String _shortenTitle(String title) {
-    if (title.length > 20) {
-      return '${title.substring(0, 20)}...(더보기)';
+    if (title.length > 25) {
+      return '${title.substring(0, 25)}...';
     } else {
       return title;
     }
@@ -82,29 +82,48 @@ class _NewsListPageState extends State<NewsListPage> {
       body: _newsList.isEmpty && _isLoading
           ? Center(child: CircularProgressIndicator())
           : ListView.builder(
-              controller: _scrollController,
-              itemCount: _newsList.length + (_hasMore ? 1 : 0),
-              itemBuilder: (context, index) {
-                if (index == _newsList.length) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                final news = _newsList[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => NewsWebViewPage(
-                          url: news.link,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 7.0, horizontal: 32.0),
+        controller: _scrollController,
+        itemCount: _newsList.length + (_hasMore ? 1 : 0),
+        itemBuilder: (context, index) {
+          if (index == _newsList.length) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final news = _newsList[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewsWebViewPage(
+                    url: news.link,
+                  ),
+                ),
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+              padding: EdgeInsets.symmetric(vertical: 12),
+              decoration: BoxDecoration(
+                color: Color(0xff3C3C3C),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 60,
+                    child: SvgPicture.asset(
+                      'assets/develop/nero-small-logo.svg',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  // Expanded를 사용하여 텍스트가 남은 공간을 차지하도록 함
+                  Expanded(
                     child: Text(
                       _shortenTitle(news.title),
+                      textAlign: TextAlign.start,
+                      softWrap: true,
                       style: TextStyle(
                         fontFamily: 'Pretendard',
                         fontWeight: FontWeight.w500,
@@ -113,9 +132,20 @@ class _NewsListPageState extends State<NewsListPage> {
                       ),
                     ),
                   ),
-                );
-              },
+                  SizedBox(width: 10),
+                  SvgPicture.asset(
+                    'assets/develop/arrow-right.svg',
+                    width: 24,
+                    height: 24,
+                    fit: BoxFit.contain,
+                  ),
+                  SizedBox(width: 5),
+                ],
+              ),
             ),
+          );
+        },
+      ),
     );
   }
 
