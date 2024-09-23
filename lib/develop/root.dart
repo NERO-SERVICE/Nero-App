af0d41dbc9a203cc1209f0eeea05e017ed97069d
pump9918 <hoyeon9918@gmail.com>
2024-09-23 22:34:17 +0900

    #16 UI: 바텀네비게이션 google_nav_bar 애니메이션 적용

--- lib/develop/root.dart
+++ lib/develop/root.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:nero_app/develop/common/controller/bottom_nav_controller.dart';
 import 'package:nero_app/develop/fastmemo/page/fast_memo_main_page.dart';
 import 'package:nero_app/develop/home/main/page/home_main_page.dart';
@@ -9,6 +10,16 @@ import 'package:nero_app/develop/todaylog/main/page/todaylog_main_page.dart';
 
 class Root extends GetView<BottomNavController> {
   const Root({super.key});
+
+  Widget get _divider => const Divider(
+        color: Color(0xff3C3C3E),
+        thickness: 1,
+        // Adjust the thickness as needed
+        height: 1,
+        // Ensure the height matches the thickness for a thin line
+        indent: 0,
+        endIndent: 0,
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -24,59 +35,59 @@ class Root extends GetView<BottomNavController> {
           ],
         );
       }),
-      bottomNavigationBar: Obx(
-            () => BottomNavigationBar(
-          currentIndex: controller.menuIndex.value,
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.transparent,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey,
-          selectedFontSize: 11.0,
-          unselectedFontSize: 11.0,
-          onTap: controller.changeBottomNav,
-          items: [
-            BottomNavigationBarItem(
-              label: '홈',
-              icon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/home-off.svg'),
-              ),
-              activeIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/home-on.svg'),
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: '하루기록',
-              icon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/todaylog-off.svg'),
-              ),
-              activeIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/todaylog-on.svg'),
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: '빠른메모',
-              icon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/fastmemo-off.svg'),
-              ),
-              activeIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/fastmemo-on.svg'),
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: '마이페이지',
-              icon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/mypage-off.svg'),
-              ),
-              activeIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/develop/mypage-on.svg'),
+      bottomNavigationBar: Container(
+        color: Color(0xff202020),
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // Column을 wrap
+          children: [
+            _divider,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Obx(
+                () => GNav(
+                  gap: 12,
+                  activeColor: Colors.white,
+                  color: Colors.grey,
+                  backgroundColor: Color(0xff202020),
+                  tabBackgroundColor: Color(0xff3C3C3C),
+                  padding: const EdgeInsets.all(16),
+                  selectedIndex: controller.menuIndex.value,
+                  onTabChange: controller.changeBottomNav,
+                  tabs: [
+                    GButton(
+                      icon: Icons.home,
+                      text: '홈',
+                      iconActiveColor: Colors.white,
+                      leading: SvgPicture.asset(controller.menuIndex.value == 0
+                          ? 'assets/develop/home-on.svg'
+                          : 'assets/develop/home-off.svg'),
+                    ),
+                    GButton(
+                      icon: Icons.calendar_today,
+                      text: '하루기록',
+                      iconActiveColor: Colors.white,
+                      leading: SvgPicture.asset(controller.menuIndex.value == 1
+                          ? 'assets/develop/todaylog-on.svg'
+                          : 'assets/develop/todaylog-off.svg'),
+                    ),
+                    GButton(
+                      icon: Icons.note_add,
+                      text: '빠른메모',
+                      iconActiveColor: Colors.white,
+                      leading: SvgPicture.asset(controller.menuIndex.value == 2
+                          ? 'assets/develop/fastmemo-on.svg'
+                          : 'assets/develop/fastmemo-off.svg'),
+                    ),
+                    GButton(
+                      icon: Icons.person,
+                      text: '마이페이지',
+                      iconActiveColor: Colors.white,
+                      leading: SvgPicture.asset(controller.menuIndex.value == 3
+                          ? 'assets/develop/mypage-on.svg'
+                          : 'assets/develop/mypage-off.svg'),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
