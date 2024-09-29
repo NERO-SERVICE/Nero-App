f7735a1427879b9fa2903113eb36939e7c58f677
pump9918 <hoyeon9918@gmail.com>
2024-09-29 18:36:10 +0900

    #16 Feat: init 페이지 ui 수정

--- lib/develop/init/page/init_start_page.dart
+++ lib/develop/init/page/init_start_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:nero_app/develop/common/components/btn.dart';
+
+import '../../common/components/custom_loading_indicator.dart';
 
 class InitStartPage extends StatelessWidget {
   final Function() onStart;
@@ -33,73 +34,13 @@ class InitStartPage extends StatelessWidget {
     );
   }
 
-  Widget _contentView() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 32.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 100),
-          const Text(
-            'By Your Side',
-            style: TextStyle(
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 8),
-          const Text(
-            '오늘도 한발짝\n스스로 돌아보며\n네로(Nero)',
-            style: TextStyle(
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w600,
-              fontSize: 44,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _startBtn() {
-    return Positioned(
-      bottom: 96,
-      left: 32,
-      right: 32,
-      child: Btn(
-        color: const Color(0xffD0EE17),
-        onTap: onStart,
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(28),
-        ),
-        child: const Center(
-          child: Text(
-            '시작하기',
-            style: TextStyle(
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w600,
-              fontSize: 16,
-              color: Colors.black,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           _backgroundView(),
-          _contentView(),
-          _startBtn(),
+          Center(child: CustomLoadingIndicator()),
         ],
       ),
     );
