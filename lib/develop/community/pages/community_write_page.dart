541afecfdacaf5fa726ee979f5e53faf3476a8a3
pump9918 <hoyeon9918@gmail.com>
2024-11-17 00:21:30 +0900

    #24 Chore: 커뮤니티 글 작성 키보드 외부 터치시 dismiss

--- lib/develop/community/pages/community_write_page.dart
+++ lib/develop/community/pages/community_write_page.dart
@@ -53,6 +53,10 @@ class _CommunityWritePageState extends State<CommunityWritePage> {
     });
   }
 
+  void _dismissKeyboard() {
+    FocusScope.of(context).unfocus();
+  }
+
   Future<void> _submitPost() async {
     if (_isSubmitting) return; // 이미 제출 중이면 함수 종료
     setState(() {
@@ -157,166 +161,169 @@ class _CommunityWritePageState extends State<CommunityWritePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        scrolledUnderElevation: 0,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        flexibleSpace: ClipRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-            child: Container(
-              color: Colors.transparent,
-            ),
-          ),
-        ),
-        toolbarHeight: 56.0,
-        titleSpacing: 0,
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.chevron_left, color: Colors.white),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            Text(
-              '게시물 작성',
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
-                color: Colors.white,
+    return GestureDetector(
+      onTap: _dismissKeyboard,
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          scrolledUnderElevation: 0,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          flexibleSpace: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+              child: Container(
+                color: Colors.transparent,
               ),
             ),
-            Obx(
-              () => _isSubmitting
-                  ? Padding(
-                      padding: EdgeInsets.only(right: 16.0),
-                      child: SizedBox(
-                        width: 24,
-                        height: 24,
-                        child: CustomLoadingIndicator(),
-                      ),
-                    )
-                  : TextButton(
-                      onPressed:
-                          (_controller.isPossibleSubmit.value && !_isSubmitting)
-                              ? _submitPost
-                              : null,
-                      child: Text(
-                        '완료',
-                        style: TextStyle(
-                          color: (_controller.isPossibleSubmit.value &&
-                                  !_isSubmitting)
-                              ? Color(0xffD0EE17)
-                              : Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
+          ),
+          toolbarHeight: 56.0,
+          titleSpacing: 0,
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.chevron_left, color: Colors.white),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              Text(
+                '게시물 작성',
+                style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+              Obx(
+                () => _isSubmitting
+                    ? Padding(
+                        padding: EdgeInsets.only(right: 16.0),
+                        child: SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: CustomLoadingIndicator(),
+                        ),
+                      )
+                    : TextButton(
+                        onPressed:
+                            (_controller.isPossibleSubmit.value && !_isSubmitting)
+                                ? _submitPost
+                                : null,
+                        child: Text(
+                          '완료',
+                          style: TextStyle(
+                            color: (_controller.isPossibleSubmit.value &&
+                                    !_isSubmitting)
+                                ? Color(0xffD0EE17)
+                                : Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                          minimumSize: Size(80, 40),
                         ),
                       ),
-                      style: TextButton.styleFrom(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                        minimumSize: Size(80, 40),
-                      ),
-                    ),
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          children: [
-            TextField(
-              controller: _contentController,
-              maxLines: 10,
-              decoration: InputDecoration(
-                hintText: '게시글 문구를 입력해주세요',
-                hintStyle: TextStyle(
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            children: [
+              TextField(
+                controller: _contentController,
+                maxLines: 10,
+                decoration: InputDecoration(
+                  hintText: '게시글 문구를 입력해주세요',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: AppColors.hintTextColor,
+                  ),
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  _controller.updateContent(value);
+                  _controller.isPossibleSubmit.value = value.isNotEmpty;
+                },
+                style: TextStyle(
                   fontFamily: 'Pretendard',
                   fontWeight: FontWeight.w400,
                   fontSize: 16,
-                  color: AppColors.hintTextColor,
+                  color: Color(0xffFFFFFF),
                 ),
-                border: InputBorder.none,
               ),
-              onChanged: (value) {
-                _controller.updateContent(value);
-                _controller.isPossibleSubmit.value = value.isNotEmpty;
-              },
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Color(0xffFFFFFF),
-              ),
-            ),
-            SizedBox(height: 16),
-            GestureDetector(
-              onTap: _selectedImages.length >= 1 ? null : _pickImages,
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                    'assets/develop/photo_gallery_icon.svg',
-                    height: 24,
-                  ),
-                  SizedBox(width: 8),
-                  Text(
-                    '사진 선택',
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                      color: Color(0xffD9D9D9),
+              SizedBox(height: 16),
+              GestureDetector(
+                onTap: _selectedImages.length >= 1 ? null : _pickImages,
+                child: Row(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/develop/photo_gallery_icon.svg',
+                      height: 24,
                     ),
-                  ),
-                  SizedBox(width: 16),
-                  Text(
-                    '${_selectedImages.length}/1',
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                      color: Color(0xffD9D9D9),
+                    SizedBox(width: 8),
+                    Text(
+                      '사진 선택',
+                      style: TextStyle(
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: Color(0xffD9D9D9),
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(width: 16),
+                    Text(
+                      '${_selectedImages.length}/1',
+                      style: TextStyle(
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: Color(0xffD9D9D9),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            _buildImagePreview(),
-            SizedBox(height: 30),
-            Text(
-              '''커뮤니티 이용 주의사항''',
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w500,
-                fontSize: 12,
-                color: Color(0xffD9D9D9),
+              SizedBox(height: 30),
+              _buildImagePreview(),
+              SizedBox(height: 30),
+              Text(
+                '''커뮤니티 이용 주의사항''',
+                style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 12,
+                  color: Color(0xffD9D9D9),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              '''1. 상호 존중: 모든 사용자는 서로를 존중하고 차별적이거나 공격적인 언어를 사용하지 않아야 합니다. 의견은 자유롭게 표현하되, 타인에게 불쾌감을 주는 표현은 삼가 주세요.\n
-2. 개인정보 보호: 개인정보(전화번호, 주소, 신분증 정보 등)를 포함한 게시물은 금지됩니다. 다른 사용자의 개인정보를 침해하지 않도록 주의해 주세요.\n
-3. 부적절한 콘텐츠 금지: 폭력적, 혐오적, 선정적이거나 불법적인 콘텐츠는 허용되지 않습니다. 또한, 외부 링크를 통한 불법 콘텐츠 유도는 금지됩니다.\n
-4. 스팸 및 광고 제한: 상업적 목적으로 작성된 글과 반복적인 스팸 게시물은 삭제될 수 있습니다.\n
-5. 건전한 대화 유도: 커뮤니티는 서로의 지식과 경험을 나누는 공간입니다. 잘못된 정보 유포를 방지하기 위해, 항상 신뢰할 수 있는 정보를 바탕으로 대화해 주세요.''',
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w400,
-                fontSize: 12,
-                color: Color(0xffD9D9D9),
+              SizedBox(height: 16),
+              Text(
+                '''1. 상호 존중: 모든 사용자는 서로를 존중하고 차별적이거나 공격적인 언어를 사용하지 않아야 합니다. 의견은 자유롭게 표현하되, 타인에게 불쾌감을 주는 표현은 삼가 주세요.\n
+      2. 개인정보 보호: 개인정보(전화번호, 주소, 신분증 정보 등)를 포함한 게시물은 금지됩니다. 다른 사용자의 개인정보를 침해하지 않도록 주의해 주세요.\n
+      3. 부적절한 콘텐츠 금지: 폭력적, 혐오적, 선정적이거나 불법적인 콘텐츠는 허용되지 않습니다. 또한, 외부 링크를 통한 불법 콘텐츠 유도는 금지됩니다.\n
+      4. 스팸 및 광고 제한: 상업적 목적으로 작성된 글과 반복적인 스팸 게시물은 삭제될 수 있습니다.\n
+      5. 건전한 대화 유도: 커뮤니티는 서로의 지식과 경험을 나누는 공간입니다. 잘못된 정보 유포를 방지하기 위해, 항상 신뢰할 수 있는 정보를 바탕으로 대화해 주세요.''',
+                style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color(0xffD9D9D9),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-          ],
+              SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
